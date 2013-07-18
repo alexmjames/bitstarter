863b8179c45a2736ea000dd81f65c52e3d68a78e
Alex M James <alexmjames.amj@gmail.com>
2013-07-18 23:16:01 +0530

    2nd try to show index.html,now Buffer is used

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-
+var buf = new Buffer(fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('index.html'));
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
